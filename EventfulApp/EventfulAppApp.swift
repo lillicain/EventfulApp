072c879edb3efb8223450ee006edb0ee75496ad9
lilli <lilliancrabbit@gmail.com>
2024-10-21 16:59:52 -0600

    Fixed

--- EventfulApp/EventfulAppApp.swift
+++ EventfulApp/EventfulAppApp.swift
@@ -6,12 +6,38 @@
 //
 
 import SwiftUI
+//import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
 
 @main
 struct EventfulAppApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    @StateObject var authenticationViewModel = AuthenticationViewModel()
+//    @StateObject var locationManager = LocationManager()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Group {
+                if authenticationViewModel.userSession == nil {
+                    PangeaView()
+                    
+                } else {
+                    TabScreen()
+                        .modifier(DarkModeViewModifier())
+                }
+            }
+            .environmentObject(authenticationViewModel)
+//            .environmentObject(locationManager)
         }
     }
 }
+
