833b45517f782fdd3b7f99a9c064a731619e85fa
lilli <lilliancrabbit@gmail.com>
2024-10-21 12:54:14 -0600

    Initial Commit

---
+++ EventfulApp/EventfulAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventfulAppApp.swift
+//  EventfulApp
+//
+//  Created by Lilli Cain on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct EventfulAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EventfulAppTests/EventfulAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  EventfulAppTests.swift
+//  EventfulAppTests
+//
+//  Created by Lilli Cain on 10/21/24.
+//
+
+import Testing
+@testable import EventfulApp
+
+struct EventfulAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
